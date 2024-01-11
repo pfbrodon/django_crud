840bb0c786d972052dc6b283ceb083b38751852a
pfbrodon <pfbrodon@gmail.com>
2024-01-11 01:30:10 -0300

    tareas OK

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length = 100)
+    descripcion=models.TextField(blank=True)
+    created= models.DateTimeField( auto_now=True)
+    datacompleted=models.DateTimeField(null=True )
+    important=models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title +'-by '+ self.user.username #muestra el titulo de la tarea en la administracion de django
