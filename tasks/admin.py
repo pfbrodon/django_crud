79f63fd9420dabeaae2ee4d1447218309e0df366
pfbrodon <pfbrodon@gmail.com>
2024-01-11 18:57:51 -0300

    ok

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Task #importa la tabla Task desde models
 
 class TaskAdmin(admin.ModelAdmin):
-    readonly_fields=('created',)
+    readonly_fields=('created',) # indico los campos de solo lectura
 # Register your models here.
 admin.site.register(Task, TaskAdmin) 
